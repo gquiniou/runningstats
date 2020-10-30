1f8b98a6f690cbda0d66a5af64c350ed5324912b
gquiniou <gquiniou@gmail.com>
2020-10-30 21:17:59 +0100

    ...

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]  [org.clojure/data.csv "1.0.0"] [clojure.java-time "0.3.2"]]
+  :dependencies [[org.clojure/clojure "1.8.0"] 
+                 [org.clojure/data.csv "1.0.0"] 
+                 [clojure.java-time "0.3.2"]]
+  :plugins [[lein-kibit "0.1.8"]
+            [lein-cljfmt "0.6.7"]]
   :main ^:skip-aot runningstats.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
